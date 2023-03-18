8b77f01e81e192f5e0e6a7d00645a0113ad9aaa0
Benjamin Catalfo <bcatalfo@gmail.com>
2023-03-18 12:40:43 -0400

    disabled ligatures

--- config.el
+++ config.el
@@ -24,8 +24,8 @@
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 ;;
-(setq doom-font (font-spec :family "Iosevka" :size 14 :weight 'normal)
-    doom-variable-pitch-font (font-spec :family "Iosevka Etoile" :size 16))
+(setq doom-font (font-spec :family "Iosevka Fixed" :size 16 :weight 'normal)
+    doom-variable-pitch-font (font-spec :family "Iosevka Fixed Slab" :weight 'normal :size 16))
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
